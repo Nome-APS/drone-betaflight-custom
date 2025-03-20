122ef91185f6eb7e92b57c13184459319c1c9bb1
giacomo2k2 <pezzolati.g@gmail.com>
2025-03-20 11:26:39 +0100

    added mamba build

---
+++ obj/main/STM32F405_OMNIBUSF4SD/lib/main/google/olc/olc.d
@@ -0,0 +1,7 @@
+obj/main/STM32F405_OMNIBUSF4SD/./lib/main/google/olc/olc.o: \
+ lib/main/google/olc/olc.c lib/main/google/olc/olc.h src/main/ctype.h \
+ lib/main/google/olc/olc_private.h lib/main/google/olc/betaflight.h
+lib/main/google/olc/olc.h:
+src/main/ctype.h:
+lib/main/google/olc/olc_private.h:
+lib/main/google/olc/betaflight.h:
---
+++ obj/main/STM32F405_OMNIBUSF4SD/startup_stm32f40xx.d
@@ -0,0 +1,2 @@
+obj/main/STM32F405_OMNIBUSF4SD/startup_stm32f40xx.o: \
+ src/main/startup/startup_stm32f40xx.s
